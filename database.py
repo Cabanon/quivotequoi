513b54375f6daddc3afc93cc9387c02222d745eb
Cabanon <Cabanon@users.noreply.github.com>
2024-03-22 23:29:17 +0000

    deploy: e346bd36dbdbf54d298a3881d4b6e4a3520ed921

--- database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sqlite"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- import.sh
+++
@@ -1,8 +0,0 @@
-for data in meps dossiers plenary_amendments votes
-do  
-    file="ep_$data.json.lz"
-    if ! curl -o $file -z $file https://parltrack.org/dumps/$file -v --stderr - | grep -q 'not new enough'
-    then
-        lzip -d $file
-    fi
-done
